3e49984665bb7aa1e0cce0c54bcc20d380f7dd0d
joseffffff <joseffff@protonmail.com>
2019-05-14 13:27:31 +0200

    cors

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from routes import Router
 import Constants
 
 def main():
     print('Starting Server')
     app = Flask(__name__)
+    CORS(app)
     router = Router.Router(app)
     router.manage_routes()
     app.run(host=Constants.HOST, port=Constants.PORT)
